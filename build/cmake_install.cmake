a2f356732d6400462032ce237c6193941ebb2562
ZachCraw <zachariahcraw@gmail.com>
2024-11-21 23:51:50 +1000

    Fixing CMake

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/thecr/Documents/Zach's Stuff/SunspotTest
+# Install script for directory: C:/Users/thecr/Documents/Zach's Stuff/Sunspot
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/thecr/Documents/Zach's Stuff/SunspotTest/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/thecr/Documents/Zach's Stuff/Sunspot/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/thecr/Documents/Zach's Stuff/SunspotTest/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/thecr/Documents/Zach's Stuff/Sunspot/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
