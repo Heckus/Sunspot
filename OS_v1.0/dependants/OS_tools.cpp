3c73dc68f15459dafb29749b542f4e84f189056a
Heckus <Hecke.Joshua@gmail.com>
2025-01-01 00:03:53 +1000

    im doing this on new years fml

--- OS_v1.0/dependants/OS_tools.cpp
+++ OS_v1.0/dependants/OS_tools.cpp
@@ -1,19 +1,138 @@
-#include "data.h"
+#include "OS_tools.h"
 
-// Constructor
-Data::Data() : thetaAngle(0.0), betaAngle(0.0), batteryLevel(100), mode(0) {
-    for (int i = 0; i < 3; ++i) {
-        buttonStates[i] = false;
+Data::Data() : thetaAngle(0), betaAngle(0), batteryLevel(0), mode(0), framerate(0), width(0), height(0), baudrate(0), serialWIREFd(-1) {
+    std::fill(std::begin(buttonStates), std::end(buttonStates), false);
+}
+
+void Data::createcamera(std::string pipeline){
+    cv::VideoCapture camera(pipeline, cv::CAP_GSTREAMER);
+    if (!camera.isOpened()) {
+        std::cout << "Error: VideoCapture not opened" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    Data.camera = camera;
+}
+
+void Data::createwriter(std::string pipeline){
+    cv::VideoWriter writer(pipeline, cv::CAP_GSTREAMER, 0, Data.framerate, cv::Size(Data.width, Data.height), true);
+    if (!writer.isOpened()) {
+        std::cerr << "Error: Could not open the video file for writing" << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    Data.writer = writer;
+}
+
+void Data::updateframe(){
+    Data.camera >> Data.currentframe;
+    if (currentframe.empty()) {
+        std::cerr << "Error: Frame is empty" << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 }
 
-// Getters
-double Data::getThetaAngle() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Data::setThetaAngle(double angle) {
+    std::lock_guard<std::mutex> lock(mtx);
+    if (angle < 90) {
+        angle = 90;
+    } else if (angle > -90) {
+        angle = -90;
+    }
+    thetaAngle = angle;
+}
+
+void Data::setBetaAngle(double angle) {
+    std::lock_guard<std::mutex> lock(mtx);
+    if (angle < 90) {
+        angle = 90;
+    } else if (angle > -90) {
+        angle = -90;
+    }
+    betaAngle = angle;
+}
+
+void Data::setBatteryLevel(int level) {
+    std::lock_guard<std::mutex> lock(mtx);
+    batteryLevel = level;
+}
+
+void Data::setMode(int mode) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->mode = mode;
+}
+
+void Data::setButtonState(int buttonIndex, bool state) {
+    if (buttonIndex >= 0 && buttonIndex < 3) {
+        std::lock_guard<std::mutex> lock(mtx);
+        buttonStates[buttonIndex] = state;
+    }
+}
+
+void Data::setLed0Color(std::string color) {
+    std::lock_guard<std::mutex> lock(mtx);
+    led0Color = color;
+}
+
+void Data::setinputpipline(std::string pipline) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->inputpipline = pipline;
+}
+
+void Data::setoutputpipline(std::string pipline) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->outputpipline = pipline;
+}
+
+void Data::setframerate(int framerate) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->framerate = framerate;
+}
+
+void Data::setwidth(int width) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->width = width;
+}
+
+void Data::setheight(int height) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->height = height;
+}
+
+void Data::setvideopath(std::string path) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->videopath = path;
+}
+
+void Data::setbaudrate(int baudrate) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->baudrate = baudrate;
+}
+
+void Data::setserialWIREFd(int fd) {
+    std::lock_guard<std::mutex> lock(mtx);
+    this->serialWIREFd = fd;
+}
+
+int Data::getThetaAngle() {
     std::lock_guard<std::mutex> lock(mtx);
     return thetaAngle;
 }
 
-double Data::getBetaAngle() {
+int Data::getBetaAngle() {
     std::lock_guard<std::mutex> lock(mtx);
     return betaAngle;
 }
@@ -29,43 +148,58 @@ int Data::getMode() {
 }
 
 bool Data::getButtonState(int buttonIndex) {
-    if (buttonIndex < 0 || buttonIndex >= 3) {
-        throw std::out_of_range("Button index out of range");
+    if (buttonIndex >= 0 && buttonIndex < 3) {
+        std::lock_guard<std::mutex> lock(mtx);
+        return buttonStates[buttonIndex];
     }
-    std::lock_guard<std::mutex> lock(mtx);
-    return buttonStates[buttonIndex];
+    return false;
 }
 
-// Setters
-void Data::setThetaAngle(double angle) {
+std::string Data::getLed0Color() {
     std::lock_guard<std::mutex> lock(mtx);
-    thetaAngle = angle;
+    return led0Color;
 }
 
-void Data::setBetaAngle(double angle) {
+std::string Data::getinputpipline() {
     std::lock_guard<std::mutex> lock(mtx);
-    betaAngle = angle;
+    return inputpipline;
 }
 
-void Data::setBatteryLevel(int level) {
+std::string Data::getoutputpipline() {
     std::lock_guard<std::mutex> lock(mtx);
-    batteryLevel = level;
+    return outputpipline;
 }
 
-void Data::setMode(int mode) {
+int Data::getframerate() {
     std::lock_guard<std::mutex> lock(mtx);
-    this->mode = mode;
+    return framerate;
 }
 
-void Data::setButtonState(int buttonIndex, bool state) {
-    if (buttonIndex < 0 || buttonIndex >= 3) {
-        throw std::out_of_range("Button index out of range");
-    }
+int Data::getwidth() {
     std::lock_guard<std::mutex> lock(mtx);
-    buttonStates[buttonIndex] = state;
+    return width;
 }
 
-// Print function
+int Data::getheight() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return height;
+}
+
+std::string Data::getvideopath() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return videopath;
+}
+
+int Data::getbaudrate() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return baudrate;
+}
+
+int Data::getserialWIREFd() {
+    std::lock_guard<std::mutex> lock(mtx);
+    return serialWIREFd;
+}
+
 void Data::printData() {
     std::lock_guard<std::mutex> lock(mtx);
     std::cout << "Theta Angle: " << thetaAngle << std::endl;
@@ -73,8 +207,17 @@ void Data::printData() {
     std::cout << "Battery Level: " << batteryLevel << std::endl;
     std::cout << "Mode: " << mode << std::endl;
     std::cout << "Button States: ";
-    for (int i = 0; i < 3; ++i) {
-        std::cout << buttonStates[i] << " ";
+    for (bool state : buttonStates) {
+        std::cout << state << " ";
     }
     std::cout << std::endl;
+    std::cout << "LED0 Color: " << led0Color << std::endl;
+    std::cout << "Input Pipeline: " << inputpipline << std::endl;
+    std::cout << "Output Pipeline: " << outputpipline << std::endl;
+    std::cout << "Framerate: " << framerate << std::endl;
+    std::cout << "Width: " << width << std::endl;
+    std::cout << "Height: " << height << std::endl;
+    std::cout << "Video Path: " << videopath << std::endl;
+    std::cout << "Baudrate: " << baudrate << std::endl;
+    std::cout << "Serial Wire FD: " << serialWIREFd << std::endl;
 }
--- OS_v1.0/dependants/OS_tools.h
+++ OS_v1.0/dependants/OS_tools.h
@@ -3,19 +3,12 @@
 
 #include <mutex>
 #include <iostream>
+#include <string>
 
 class Data {
 public:
     // Constructor
     Data();
-
-    // Getters
-    double getThetaAngle();
-    double getBetaAngle();
-    int getBatteryLevel();
-    int getMode();
-    bool getButtonState(int buttonIndex);
-    string getLed0Color();
 
     // Setters
     void setThetaAngle(double angle);
@@ -25,19 +18,37 @@ public:
     void setButtonState(int buttonIndex, bool state);
     void setLed0Color(string color);
 
+    void setinputpipline(std::string pipline);
+    void setoutputpipline(std::string pipline);
+    void setframerate(int framerate);
+    void setwidth(int width);
+    void setheight(int height);
+    void setvideopath(std::string path);
+    void setbaudrate(int baudrate);
+    void setserialWIREFd(int fd);
+
     // Print function
     void printData();
 
 private:
     double thetaAngle;
     double betaAngle;
-    string led0Color;
+    std::string led0Color;
     int batteryLevel;
     int mode;
     bool buttonStates[3]; // Assuming there are 10 buttons
 
+    std::string inputpipline;
+    std::string outputpipline;
     int width;
     int height;
+    int framerate;
+    std::string videopath;
+
+    int baudrate;
+    std::string serialwiredevice = "/dev/ttyAMA0";
+    std::string boot = "BOOT";
+    std::string reset = "RESET";
 
     volatile int serialWIREFd;
     std::mutex mtx; // Mutex for thread safety
