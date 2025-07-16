f6b98c5a2b78be81ce91cf21f4e65cdd6149ee90
Heckus <hecke.joshua@gmail.com>
2025-07-16 12:42:43 +1000

    ros2_ws work models

--- PV1/ros2_ws/src/ball_tracker/setup.py
+++ PV1/ros2_ws/src/ball_tracker/setup.py
@@ -1,43 +1,40 @@
 import os
 from glob import glob
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'ball_tracker'
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    version='0.0.0',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files from the 'launch' directory
+        # --- This section installs all your resource files ---
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        # Include all config files from the 'config' directory
-        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
-        # Include all model files from the 'models' directory
-        (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.pt'))),
-        
-        # --- ADD THESE TWO LINES ---
         (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
-        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.dae'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.dae'))), # Change to .stl if needed
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'models'), glob(os.path.join('models', '*.pt'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='hecke',
-    maintainer_email='hecke@todo.todo',
-    description='A 3D ball tracker pipeline.',
-    license='Apache License 2.0',
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='Ball tracker package',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'camera_node = ball_tracker.camera_node:main',
             'detection_node = ball_tracker.detection_node:main',
             'calculation_node = ball_tracker.calculation_node:main',
-            'extrinsic_calibrator = ball_tracker.extrinsic_calibration_node:main',
-            'battery_monitor = ball_tracker.battery_monitor_node:main',
-            'frame_saver = ball_tracker.frame_saver_node:main',
+            'extrinsic_calibrator = ball_tracker.extrinsic_calibrator:main',
+            'frame_saver = ball_tracker.frame_saver:main',
+            'battery_monitor = ball_tracker.battery_monitor:main',
         ],
     },
 )
